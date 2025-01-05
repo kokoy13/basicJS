83dab73379a9ae772a6335ca38135aba7a4827b1
kokoy13 <andikafiransyah1905@gmail.com>
2025-01-05 21:14:14 +0700

    error handling

---
+++ error/index.js
@@ -0,0 +1,7 @@
+try {
+    console.log('Memulai program');
+    throw new Error('Error: Program berhenti');
+    console.log('Mengakhiri program');
+  } catch (err) {
+    console.log('Karena ada error, blok ini akan dieksekusi');
+  }
