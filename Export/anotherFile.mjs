1264018f7e0ee9fe30a5470b0ba22624250a6138
kokoy13 <andikafiransyah1905@gmail.com>
2025-01-06 10:49:39 +0700

    module

---
+++ Export/anotherFile.mjs
@@ -0,0 +1,5 @@
+const nama = 'Andika';
+const usia = 21;
+const lolos = true;
+
+export {nama, usia, lolos};
---
+++ Export/index.mjs
@@ -0,0 +1,5 @@
+import {nama, usia, lolos} from './anotherFile.mjs';
+//atau
+import * as all from './anotherFile.mjs'
+
+console.log(all.usia);
